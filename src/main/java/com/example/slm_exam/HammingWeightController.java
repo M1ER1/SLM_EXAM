f018edf592caddcd5f74b99bb998cc55e1db7acc
M1ER1 <131255946+M1ER1@users.noreply.github.com>
2023-06-13 12:16:45 +0200

    1-User-Story implementiert

--- src/main/java/com/example/slm_exam/HammingWeightController.java
+++ src/main/java/com/example/slm_exam/HammingWeightController.java
@@ -1,4 +1,25 @@
 package com.example.slm_exam;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HammingWeightController {
+
+    @RequestMapping("/api/hamming")
+    public int calculateHammingWeight(@RequestParam String number) {
+        String letzteEingabe = number; //speichert den String (wird für die nächste User-Story gebraucht)
+
+        int HammingWeight = 0;
+
+        for (int i = 0; i < number.length(); i++) {
+            if (number.charAt(i) != '0') {
+                HammingWeight++;
+            }
+        }
+
+        return HammingWeight;
+    }
 }
+
